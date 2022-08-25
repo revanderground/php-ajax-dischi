ada377ae04d7daccdc7d459f7deebe61bf2c0b8c
vtnboolean <vnthuanx@gmail.com>
2022-08-25 16:58:07 +0200

    logo path fixed

--- Prima Versione/index.php
+++ Prima Versione/index.php
@@ -9,7 +9,6 @@ Utilizziamo: Html, CSS, VueJS (importato tramite CDN), axios, PHP
 
 Prima Versione:
 Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi.
-
 Seconda Versione:
 Attraverso l’utilizzo di axios: al caricamento della pagina axios chiederà, attraverso una chiamata api, i dischi a php e li stamperà attraverso vue.
 
