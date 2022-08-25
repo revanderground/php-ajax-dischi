15023cf42db78dd84bf2243dc03e21423aa93200
vtnboolean <vnthuanx@gmail.com>
2022-08-25 16:34:53 +0200

    creazione api

--- Prima Versione/index.php
+++ Prima Versione/index.php
@@ -2,15 +2,20 @@
 
 <!-- Esercizio di oggi: PHP Ajax Dischi
 cartella/repo: php-ajax-dischi
+
 Descrizione:
 Stampare a schermo una decina di dischi musicali (vedi screenshot).
 Utilizziamo: Html, CSS, VueJS (importato tramite CDN), axios, PHP
+
 Prima Versione:
 Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi.
+
 Seconda Versione:
 Attraverso l’utilizzo di axios: al caricamento della pagina axios chiederà, attraverso una chiamata api, i dischi a php e li stamperà attraverso vue.
+
 Prima Milestone: recuperare la lista dei dischi tramite una chiamata axios e stampare la lista dei titoli degli album all'interno di una lista non numerata <ul>
 Seconda Milestone: per ogni disco, stampare una card con tutte le informazioni
+
 Bonus:
 Attraverso un’altra chiamata api, filtrare gli album per genere lato backend.
 Consigli del giorno:
